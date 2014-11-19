011924a2593b3d02f18342ff2426387cacf339e5
Toine Hartman <tjbhartman@gmail.com>
2014-11-19 15:59:27 +0100

    Expanded Player class.
    Player has properties and getters and setters for those.
    Equals method and toString method added.

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,77 @@
-
+/**
+ * Player class.
+ * 
+ * Represents a football player.
+ * 
+ * @author Toine Hartman <tjbhartman@gmail.com>
+ */
 public class Player {
-	//Hello2
+	// Properties of a player
+	private String name;
+	private int offensive;
+	private int defensive;
+	private int stamina;
+	
+	public Player(String name, int off, int def, int sta) {
+		setName(name);
+		setOffensive(off);
+		setDefensive(def);
+		setStamina(sta);
+	}
+	
+	/**
+	 * Checks if this is equal to other.
+	 */
+	public boolean equals(Object other) {
+		if (other instanceof Player) {
+			Player that = (Player)other;
+			
+			return this.name.equals(that.name) &&
+					this.offensive == that.offensive &&
+					this.defensive == that.defensive &&
+					this.stamina == that.stamina;
+		}
+		
+		return false;
+	}
+	
+	// TODO add javadoc
+	public String toString() {
+		String s = this.name;
+		s += String.format(": (%d, %d, %d)", getOffensive(), getDefensive(), getStamina());
+		
+		return s;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getOffensive() {
+		return this.offensive;
+	}
+	
+	public void setOffensive(int off) {
+		this.offensive = off;
+	}
+	
+	public int getDefensive() {
+		return this.defensive;
+	}
+	
+	public void setDefensive(int def) {
+		this.defensive = def;
+	}
+	
+	public int getStamina() {
+		return this.stamina;
+	}
+	
+	public void setStamina(int sta) {
+		this.stamina = sta;
+	}
 }
