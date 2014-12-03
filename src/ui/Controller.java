7570e81a8f9d96ab8252fa9869251b52f067aace
Toine Hartman <tjbhartman@gmail.com>
2014-12-03 16:15:12 +0100

    Improved Save and Load buttons.
    Added ID to Save and Soad buttons.
    Added Save and Load nuttons in code.
    Added onAction event to Save button programmatically.

--- src/ui/Controller.java
+++ src/ui/Controller.java
@@ -1,19 +1,27 @@
-/**
- * 
- */
 package ui;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 /**
  * @author Toine Hartman <tjbhartman@gmail.com>
  *
  */
 public class Controller implements Initializable {
+	
+	@FXML
+	private Button saveGameButton;
+	
+	@FXML
+	private Button loadGameButton;
 
 	/* (non-Javadoc)
 	 * @see javafx.fxml.Initializable#initialize(java.net.URL, java.util.ResourceBundle)
@@ -21,11 +29,26 @@ public class Controller implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-
+		saveGameButton.setOnAction((event) -> {
+			System.out.println(event.getSource());
+			saveGame(event);
+		});
 	}
 	
-	public void press(ActionEvent event) {
-		System.out.println("Button was pressed!");
+	@FXML
+	public void saveGame(ActionEvent event) {
+		FileChooser chooser = new FileChooser();
+		chooser.setTitle("Save game location");
+		configureFileChooser(chooser);
+		
+		//TODO fix stage binding
+		File selectedFile = chooser.showOpenDialog(null);
+		System.out.println("File: " + selectedFile.getAbsolutePath());
+	}
+	
+	public void configureFileChooser(FileChooser fc) {
+		fc.setInitialDirectory(new File(System.getProperty("user.home")));
+		fc.setSelectedExtensionFilter(new ExtensionFilter("XML Game files", ".xml"));
 	}
 
 }
