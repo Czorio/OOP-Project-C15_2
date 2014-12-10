1e75e95707a8ae33315abc457d0c8c0e42db0d44
TH3steven <stevenmeijer9@gmail.com>
2014-12-10 16:05:21 +0100

    Added logic to calculate a player's offensive stat, defensive stat and stamina. Small cleanup

--- src/logic/PlayerLogic.java
+++ src/logic/PlayerLogic.java
@@ -3,42 +3,91 @@ package logic;
 import dataModel.Player;
 
 /**
- * Class to calculate a players statistics
+ * Class to calculate a players statistics.
  * @author Steven Meijer <stevenmeijer9@gmail.com>
  *
  */
 public final class PlayerLogic {
-	
-	private PlayerLogic(){ 
-		
-	}
-	
-	public static int calculatePlayerOffScore(Player player){
-		int output, posModifier = 0;
-		if(player.getPosition().equals(player.getCurPosition())){
-			posModifier = 1;
+
+	public PlayerLogic() { }
+
+	/**
+	 * Calculates a player's offensive score by their position.
+	 * @param player The player to calculate the score for.
+	 * @return Returns the offensive score of a player.
+	 */
+	public static final int calculatePlayerOffScore(Player player){
+		int playerOff = player.getOffensive();
+		String curPosition = player.getCurPosition();
+
+		if (curPosition == "Attacker") {
+			playerOff = (int) Math.round(player.getOffensive() * 1.25);
 		}
-		
-		output = player.getOffensive() * posModifier;
-		
-		return output;
-	}
-	
-	public int calculatePlayerDefScore(Player player){
-		int output, posModifier = 0;
-		if(player.getPosition() == player.getCurPosition()){
-			posModifier = 1;
+
+		else if (curPosition == "Defender") {
+			playerOff = (int) Math.round(player.getOffensive() * 0.75);
 		}
-		
-		output = player.getDefensive() * posModifier;
-		
-		return output;
+
+		return playerOff;
 	}
-	
-//	public int calculatePlayerStamina(){
-//		int staminaDiscount = playedGames * 2;
-//		
-//		stamina -= staminaDiscount;
-//		return stamina;
-//	}
+
+	/**
+	 * Calculates a player's defensive score by their position.
+	 * @param player The player to calculate the score for.
+	 * @return Returns the defensive score of a player.
+	 */
+	public static final int calculatePlayerDefScore(Player player){
+		int playerDef = player.getDefensive();
+		String curPosition = player.getCurPosition();
+
+		if (curPosition == "Attacker") {
+			playerDef = (int) Math.round(player.getDefensive() * 0.75);
+		}
+
+		else if (curPosition == "Defender") {
+			playerDef = (int) Math.round(player.getDefensive() * 1.25);
+		}
+
+		return playerDef;
+	}
+
+	/**
+	 * Calculates the amount of games a player has played.
+	 * @param player The player to calculate the number of games for.
+	 * @return Returns the amound of games played by a certain player.
+	 */
+	public static final int calculatePlayedGames(Player player) {
+		int playedGames = player.getPlayedGames();
+
+		//TODO Implement played games per player
+		return playedGames;
+	}
+
+	/**
+	 * Calculates a player's stamina.
+	 * @param player The player to calculate the stamina for.
+	 * @return Returns the stamina of a player.
+	 */
+	public static final int calculatePlayerStamina(Player player){
+		int stamina = player.getStamina(), result = 0;
+
+		if (stamina >= 80) {
+			result = stamina - (3 * player.getPlayedGames());
+		}
+
+		else if (stamina < 80 && stamina >= 65) {
+			result = stamina - (4 * player.getPlayedGames());
+		}
+
+		else if (stamina < 65 && stamina >= 40) {
+			result = stamina - (5 * player.getPlayedGames());
+		}
+
+		else if (stamina < 40) {
+			result = stamina - (6 * player.getPlayedGames());
+		}
+
+		return result;
+	}
+
 }
