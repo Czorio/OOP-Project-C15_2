ddaccd7c5da61f83f8950c1c13905024fc37b0b3
borisroman <boris@radialcontext.nl>
2014-12-16 23:27:55 +0100

    Initial MarketplaceLogic file.

---
+++ src/logic/MarketplaceLogic.java
@@ -0,0 +1,13 @@
+package logic;
+
+/**
+ * Logic for the marketplace. Includes methods for determining what players are offered a contract and for which price. Also has functions to change players from teams. 
+ * @author Boris Schrijver <boris@radialcontext.nl>
+ */
+public class MarketplaceLogic {
+
+}
+
+// @TODO Implement change player function.
+// @TODO Implement offer function.
+// @TODO Implement warning checks for budget, team count etc.
