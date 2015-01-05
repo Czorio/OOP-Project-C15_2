5dab861af8a882df35094fd1221d5a99345bff92
Mathijs de Boer <czorio4@gmail.com>
2015-01-05 09:20:56 +0100

    Added missing Controllers
    Signed-off-by: Mathijs de Boer <czorio4@gmail.com>

---
+++ src/ui/MatchViewController.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package ui;
+
+import java.net.URL;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+/**
+ * @author Mathijs de Boer <czorio4@gmail.com>
+ *
+ */
+public class MatchViewController implements Initializable, Observer {
+
+	/* (non-Javadoc)
+	 * @see java.util.Observer#update(java.util.Observable, java.lang.Object)
+	 */
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see javafx.fxml.Initializable#initialize(java.net.URL, java.util.ResourceBundle)
+	 */
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
