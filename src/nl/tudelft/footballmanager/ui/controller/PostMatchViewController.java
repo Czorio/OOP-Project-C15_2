b92bf6b239684fb268efd006cbf71880d0a2a2d3
Toine Hartman <tjbhartman@gmail.com>
2015-01-21 14:09:58 +0100

    Altered PostMatchView
    Own team is always on top and highlighted

--- src/nl/tudelft/footballmanager/ui/controller/PostMatchViewController.java
+++ src/nl/tudelft/footballmanager/ui/controller/PostMatchViewController.java
@@ -20,18 +20,19 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.util.Callback;
 import nl.tudelft.footballmanager.FootballManager;
 import nl.tudelft.footballmanager.model.GameState;
 import nl.tudelft.footballmanager.model.Match;
-import nl.tudelft.footballmanager.model.Player;
 import nl.tudelft.footballmanager.model.logic.GameLogic;
 import nl.tudelft.footballmanager.model.logic.MarketplaceLogic;
-import nl.tudelft.footballmanager.model.logic.TeamLogic;
+
 
 /**
  * @author Mathijs de Boer <czorio4@gmail.com>
@@ -51,6 +52,27 @@ public class PostMatchViewController implements Initializable, Observer {
 
 	private static GameState gameState = null;
 
+	private static Callback<TableColumn<Match, String>, TableCell<Match, String>> highlightMyTeam = new Callback<TableColumn<Match, String>, TableCell<Match, String>>() {
+		@Override
+		public TableCell<Match, String> call(TableColumn<Match, String> param) {
+			return new TableCell<Match, String>() {
+				@Override
+				protected void updateItem(String teamName, boolean empty) {
+					if (teamName == null || empty) {
+						this.setText(null);
+						this.setTextFill(Color.WHITE);
+					} else if (teamName.equals(gameState.getMyTeamName())) {
+						this.setText(teamName);
+						this.setTextFill(Color.RED);
+					} else {
+						this.setText(teamName);
+						this.setTextFill(Color.BLACK);
+					}
+				}
+			};
+		}
+	};
+
 	@Override
 	public void update(Observable o, Object arg) {
 
@@ -63,6 +85,14 @@ public class PostMatchViewController implements Initializable, Observer {
 		int gameRound = gameState.getGameRound();
 		try {
 			List<Match> matches = gameState.getMatchScheme().getMatchdays().get(gameRound).getMatches();
+			for (int i = 0; i < matches.size(); i++) {
+				Match m = matches.get(i);
+				if (m.getHome().getName().equals(gameState.getMyTeamName()) || m.getAway().getName().equals(gameState.getMyTeamName())) {
+					Match tmp = matches.get(0);
+					matches.set(0, m);
+					matches.set(i, tmp);
+				}
+			}
 			playedMatchesTableView.setItems(FXCollections.observableList(matches));
 		} catch (IndexOutOfBoundsException e) {
 			PostLeagueViewController.show(gameState);
@@ -71,9 +101,9 @@ public class PostMatchViewController implements Initializable, Observer {
 		continueButton.setOnAction((event) -> {
 			// After the match, players will be trasfered, so they will be visible when the RootViewController is showed.
 			MarketplaceLogic.RandomPlayerTransfer(gameState.getGameRound(), gameState.getLeague(), gameState.getMyTeam());
-			
+
 			RootViewController.show(gameState);
-			
+
 
 		});
 
@@ -83,11 +113,15 @@ public class PostMatchViewController implements Initializable, Observer {
 			}
 		});
 
+		playedMatchesTeam1TableColumn.setCellFactory(highlightMyTeam);
+
 		playedMatchesTeam2TableColumn.setCellValueFactory(new Callback<CellDataFeatures<Match, String>, ObservableValue<String>>() {
 			public ObservableStringValue call(CellDataFeatures<Match, String> p) {
 				return new SimpleStringProperty(p.getValue().getAway().getName());
 			}
 		});
+
+		playedMatchesTeam2TableColumn.setCellFactory(highlightMyTeam);
 
 		playedMatchesScoreTableColumn.setCellValueFactory(new Callback<CellDataFeatures<Match, String>, ObservableValue<String>>() {
 			public ObservableStringValue call(CellDataFeatures<Match, String> p) {
@@ -99,19 +133,19 @@ public class PostMatchViewController implements Initializable, Observer {
 		GameLogic.matchDay();
 
 		System.out.println("Initializing " + this.getClass() + " finished");
-	}
+}
 
-	public static void show(GameState gs) {
-		gameState = gs;
+public static void show(GameState gs) {
+	gameState = gs;
 
-		FXMLLoader l = new FXMLLoader();
-		l.setLocation(FootballManager.class.getResource(postMatchViewFileName));
-		try {
-			AnchorPane postMatchView = (AnchorPane) l.load();
-			FootballManager.getStage().setScene(new Scene(postMatchView));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	FXMLLoader l = new FXMLLoader();
+	l.setLocation(FootballManager.class.getResource(postMatchViewFileName));
+	try {
+		AnchorPane postMatchView = (AnchorPane) l.load();
+		FootballManager.getStage().setScene(new Scene(postMatchView));
+	} catch (IOException e) {
+		e.printStackTrace();
 	}
+}
 
 }
