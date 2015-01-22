1c16334fda45a439dd6fc970ca1a8bdd0f0c1304
TH3steven <stevenmeijer9@gmail.com>
2015-01-22 23:48:43 +0100

    Started base for random player bidding.

--- src/nl/tudelft/footballmanager/model/logic/MarketplaceLogic.java
+++ src/nl/tudelft/footballmanager/model/logic/MarketplaceLogic.java
@@ -1,18 +1,24 @@
 package nl.tudelft.footballmanager.model.logic;
 
+import java.util.List;
+
+import nl.tudelft.footballmanager.model.GameState;
 import nl.tudelft.footballmanager.model.League;
 import nl.tudelft.footballmanager.model.Player;
 import nl.tudelft.footballmanager.model.Team;
 
-
 /**
- * Logic for the marketplace. Includes methods for determining what players are offered a contract and for which price. Also has functions to change players from teams. 
+ * Logic for the marketplace. 
+ * Includes methods for determining what players are offered a contract and for which price. 
+ * Has functions to change players from teams. 
+ * Gives the user a random bid on a random player.
  * @author Boris Schrijver <boris@radialcontext.nl>
  */
 public final class MarketplaceLogic {
 	private MarketplaceLogic(){};
 	
 	/**
+	 * Handles transfering of players.
 	 * @param fromTeam	Team from which the player needs to be removed.
 	 * @param toTeam	Team to which the players needs to be moved.
 	 * @param player	The player
@@ -49,6 +55,7 @@ public final class MarketplaceLogic {
 	
 
 	/**
+	 * Transfers a random player to a random team.
 	 * @param currentRound	The round we currently play in.
 	 * @param league		The league object.
 	 * @param myTeam		The team you're coaching.
@@ -97,13 +104,47 @@ public final class MarketplaceLogic {
 		}		
 	}
 	
+	/**
+	 * Offers the bid to the player.
+	 * @param team The team that offers the bid.
+	 * @param player The player they bid on.
+	 * @param price The price they are willing to pay.
+	 * @param gs The current gamestate.
+	 * @return 
+	 */
+	public static final boolean giveBid(Team team, Player player, int price, GameState gs) {
+		//TODO Offer the bid to the player.
+		
+		if(isTransferWindow(1)/*Change to: The player accepts the bid*/) {
+			transferPlayer(team, gs.getMyTeam(), player, gs.getGameRound());
+			return true;
+		}
+		
+		return false;
+	}
 	
+	/**
+	 * A random team bids on a random player, for a semi random price.
+	 * @param gs The current gamestate.
+	 */
+	public static final void randomBid(GameState gs) {
+		List<Team> teams = gs.getLeague().getTeams();
+		Team biddingTeam = teams.get(GameLogic.generateRandom(0, teams.size() - 1));
+		
+		List<Player> userPlayers = gs.getMyTeam().getPlayers();
+		Player p = userPlayers.get(GameLogic.generateRandom(0, userPlayers.size() - 1));
+		
+		int price = p.getPrice() + GameLogic.generateRandom(0, 300000) - GameLogic.generateRandom(0, 300000);
+		
+		giveBid(biddingTeam, p, price, gs);
+	}
 	
+	/**
+	 * Handles opening and closing of the transfer window.
+	 * Transfer window is opened the first 4 rounds and between round 18 and 20.
+	 * @return Whether or not the transfer window is open or closed.
+	 */
 	public static final boolean isTransferWindow(int currentRound) {
-		// Transfermarkt is open van 1 juli tot en met 31 augustus,
-		// welke overeenkomt met de eerste 4 weken van de Eredivisie.
-		// De 2de termijn is van 1 januari tot en met 31 januari,
-		// welke overeenkomt met de 18de tot en met 20ste speelronde
 		if(currentRound >= 1 && currentRound <= 4)
 			return true;
 		else if(currentRound >= 18 && currentRound <= 20)
